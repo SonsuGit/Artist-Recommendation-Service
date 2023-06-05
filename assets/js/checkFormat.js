3f0c66def610da2901e016b6da13e704c33cd607
SonSuha <dhkd98@gmail.com>
2023-06-05 13:18:07 +0900

    annotations

--- assets/js/checkFormat.js
+++ assets/js/checkFormat.js
@@ -1,10 +1,10 @@
+// ChatGPT API가 원하는 포맷(이름,이름,이름,이름)으로 데이터를 줬는지 확인하는 함수
 /**
- * 전달해준 답이 format에 맞는지 판단함
- * @param {object} response ChatGPT가 전달해준 답
- * @returns 그 답이 빈칸이 아닌 4개의 이름인지 아닌지 Boolean
+ * 포멧을 확인하는 함수
+ * @param {object} response ChatGPT가 주는 resoponse
+ * @returns 포멧에 맞는지 Boolean 값 return
  */
 function checkFormat(response){
-    const names = response.choices[0].message.content
-    .split(',').map(name => name.trim());
+    const names = response.choices[0].message.content.split(',').map(name => name.trim());
     return names.length === 4 && names.every(name => name !== '');
 };
